47569a772bc0b942b400f62598bb60f63132428e
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-02-11 15:04:29 -0300

    fix: if stockTotal is zero return Bad Request

--- src/schemas/game.schema.js
+++ src/schemas/game.schema.js
@@ -3,7 +3,7 @@ import joi from "joi";
 const gameSchema = joi.object({
   name: joi.string().required(),
   image: joi.string().uri().required(),
-  stockTotal: joi.number().required(),
+  stockTotal: joi.number().positive().required(),
   pricePerDay: joi.number().required()
 });
 
