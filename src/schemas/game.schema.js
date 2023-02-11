4cbaea3208b9628443c4a99a1d7ccb79fe96137d
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-02-11 16:40:55 -0300

    fix: update gameSchema

--- src/schemas/game.schema.js
+++ src/schemas/game.schema.js
@@ -4,7 +4,7 @@ const gameSchema = joi.object({
   name: joi.string().required(),
   image: joi.string().uri().required(),
   stockTotal: joi.number().positive().required(),
-  pricePerDay: joi.number().required()
+  pricePerDay: joi.number().positive().required()
 });
 
 export default gameSchema;
