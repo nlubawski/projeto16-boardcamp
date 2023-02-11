2223e3407d775b3fcc994d2091b87b174aca8335
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-02-11 14:56:47 -0300

    feat: return conflict if name game alrady exist

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -12,7 +12,8 @@ export async function getGames(req, res){
 export async function postGames(req, res){
   const {name, image, stockTotal, pricePerDay} = req.body
   try { 
-    
+    const haveGame = await db.query(`SELECT * FROM games WHERE name=$1`,[name])
+    if(haveGame.rows.length > 0) return res.sendStatus(409)
     await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") 
     VALUES ($1, $2, $3, $4)`, [name, image, stockTotal, pricePerDay])
     return res.sendStatus(201)
