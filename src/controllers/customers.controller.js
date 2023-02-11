db34692deb70582c0ff185be29ac9b766bcf7407
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-02-11 15:31:13 -0300

    feat: create getCustomersById

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -7,5 +7,15 @@ export async function getCustomers(req, res){
   } catch (error) {
     return res.sendStatus(500)
   }
+}
 
+export async function getCustomersById(req, res){
+  const {id} = req.params
+  try { 
+    const customer = await db.query(`SELECT * FROM customers WHERE id=$1`,[id])
+    if(customer.rows <= 0 ) return res.sendStatus(404)
+    return res.send(customer.rows)
+  } catch (error) {
+    return res.sendStatus(500)
+}
 }
--- src/routes/customers.router.js
+++ src/routes/customers.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getCustomers } from "../controllers/customers.controller.js";
+import { getCustomers, getCustomersById } from "../controllers/customers.controller.js";
 
 const customersRouter = Router()
 customersRouter.get('/customers', getCustomers)
+customersRouter.get('/customer/:id', getCustomersById)
 
 
 export default customersRouter
