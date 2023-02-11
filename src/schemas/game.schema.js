f72303f24e3cfed56ad8bbb65b2cad3fdb042c4c
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-02-11 14:32:30 -0300

    feat: create gameSchema

---
+++ src/schemas/game.schema.js
@@ -0,0 +1,10 @@
+import joi from "joi";
+
+const gameSchema = joi.object({
+  name: joi.string().required(),
+  image: joi.string().uri().required(),
+  stockTotal: joi.number().required(),
+  pricePerDay: joi.number().required()
+});
+
+export default gameSchema;
