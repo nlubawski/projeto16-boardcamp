0f0337ef96b61f184a299f7486f38aa4ad218ba7
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-02-11 14:52:36 -0300

    fix: gameSchema

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -12,6 +12,7 @@ export async function getGames(req, res){
 export async function postGames(req, res){
   const {name, image, stockTotal, pricePerDay} = req.body
   try { 
+    
     await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") 
     VALUES ($1, $2, $3, $4)`, [name, image, stockTotal, pricePerDay])
     return res.sendStatus(201)
--- src/middlewares/games.middlewares.js
+++ src/middlewares/games.middlewares.js
@@ -2,7 +2,7 @@ import gameSchema from "../schemas/game.schema.js";
 
 export function gameValidation(req, res, next){
   const game = req.body
-  const validation = gameSchema.validateAsync(game)
-  if(validation.error) return res.sendStatus(400)
+  const {error} = gameSchema.validate(game)
+  if(error) return res.sendStatus(400)
   next()
 }
