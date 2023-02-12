77cf31a235952fc3473a75b29e9306ce1cd50334
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-02-12 12:27:55 -0300

    fix: delete route

--- src/routes/rentals.router.js
+++ src/routes/rentals.router.js
@@ -6,6 +6,6 @@ const rentalsRouter = Router()
 rentalsRouter.get('/rentals', getRentals)
 rentalsRouter.post('/rentals', rentalValidation, postRentals)
 rentalsRouter.post('/rentals/:id/return', finishRental)
-rentalsRouter.delete('/rentals:/id', deleteRental)
+rentalsRouter.delete('/rentals/:id', deleteRental)
 
 export default rentalsRouter
