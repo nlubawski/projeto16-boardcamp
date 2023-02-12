e001103ea5fec3ed6ce0c46aa0e2d6169fbaf355
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-02-12 12:07:08 -0300

    feat: create rentalSchema and rentalValidation

--- src/routes/rentals.router.js
+++ src/routes/rentals.router.js
@@ -1,9 +1,10 @@
 import {Router} from 'express'
 import { getRentals, postRentals } from '../controllers/rentals.controllers.js'
+import { rentalValidation } from '../middlewares/rentals.middleware.js'
 
 const rentalsRouter = Router()
 rentalsRouter.get('/rentals', getRentals)
-rentalsRouter.post('/rentals', postRentals)
+rentalsRouter.post('/rentals', rentalValidation, postRentals)
 rentalsRouter.post('/rentals/:id/return')
 
 export default rentalsRouter
