0a2539138418e223dd2260f301b4d2ea543ab7f5
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-02-12 12:03:10 -0300

    feat: create finishRental

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -2,8 +2,10 @@ import db from './../database/database.connection.js';
 
 export async function getRentals(req, res) {
   try {
-    const result = await db.query({text: `SELECT rentals.*, customers.name AS customer, games.name FROM rentals JOIN customers ON customers.id = rentals."customerId" 
-    JOIN games ON games.id = rentals."gameId"`, rowMode: "array"})
+    const result = await db.query({
+      text: `SELECT rentals.*, customers.name AS customer, games.name FROM rentals JOIN customers ON customers.id = rentals."customerId" 
+    JOIN games ON games.id = rentals."gameId"`, rowMode: "array"
+    })
     return res.send(result.rows.map(rentalToObject))
   } catch (error) {
     return res.sendStatus(500)
@@ -25,6 +27,27 @@ export async function postRentals(req, res) {
       INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, NOW(), $3, null, $4, null)`, [customerId, gameId, daysRented, originalPrice]);
     console.log('aqui nao')
     return res.sendStatus(201)
+  } catch (error) {
+    return res.sendStatus(500)
+  }
+}
+
+export async function finishRental(req, res) {
+  const { id } = req.params
+  try {
+    const rentalId = await db.query(`SELECT * FROM rentals WHERE id = $1`, [id]);
+    if (rentalId.rows.length === 0) return res.sendStatus(404)
+    const rental = rentalId.rows[0];
+    if (rental.returnDate) return res.sendStatus(400);
+    const diference = new Date().getTime() - new Date(rental.rentDate).getTime();
+    const diferenceInDays = Math.floor(diference / (24 * 3600 * 1000));
+    let delayFee = 0;
+    if (diferenceInDays > rental.daysRented) {
+      const addicionalDays = diferenceInDays - rental.daysRented;
+      delayFee = addicionalDays * rental.originalPrice;
+    };
+    await db.query(`UPDATE rentals SET "returnDate" = NOW(), "delayFee" = $1 WHERE id = $2`, [delayFee, id]);
+    return res.sendStatus(200)
   } catch (error) {
     return res.sendStatus(500)
   }
--- src/routes/rentals.router.js
+++ src/routes/rentals.router.js
@@ -4,5 +4,6 @@ import { getRentals, postRentals } from '../controllers/rentals.controllers.js'
 const rentalsRouter = Router()
 rentalsRouter.get('/rentals', getRentals)
 rentalsRouter.post('/rentals', postRentals)
+rentalsRouter.post('/rentals/:id/return')
 
 export default rentalsRouter
